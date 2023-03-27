36307f63d0ebda922854fd4b040df7da0ea03754
Andrew Nyzhnyk <andrew@awkwrd.dev>
2023-03-27 20:25:08 +0200

    added constants

---
+++ src/types/consts.ts
@@ -0,0 +1,8 @@
+export const Author = 'Andrew Nyzhnyk';
+
+export enum Links {
+  RESUME = 'https://cv.awkwrd.dev',
+  BLOG = 'https://dasph.medium.com',
+  GITHUB = 'https://github.com/dasph',
+  LINKEDIN = 'https://www.linkedin.com/in/andrew-nyzhnyk/'
+}
