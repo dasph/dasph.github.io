5b15f0e740099ed33096b2a8e99bd6b81d713241
Andrew Nyzhnyk <andrew@awkwrd.dev>
2023-05-11 18:46:01 +0200

    updated styles

--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -3,7 +3,7 @@ import type { ParentComponent } from 'solid-js';
 import styles from './.module.scss';
 
 export const Default: ParentComponent = ({ children }) => (
-  <div class={styles.div}>
+  <div class={styles.module}>
     {children}
   </div>
 )
