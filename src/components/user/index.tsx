5017539d6128d30e0ac34e94e4f5378363f8c956
Andrew Nyzhnyk <andrew@awkwrd.dev>
2023-05-11 18:46:35 +0200

    updated user component

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,19 +1,32 @@
-import type { Component } from 'solid-js';
-import { A } from '@solidjs/router';
+import { Component, createSignal, For, onMount } from 'solid-js';
 
 import styles from './.module.scss';
 
-import { avatar } from '@assets';
 import { Image } from '@components';
-import { Author } from '@types';
+import { Author, Links } from '@types';
+import { avatar, Email, Github, Linkedin } from '@assets';
 
 type Props = {
   className?: string;
 };
 
-export const User: Component<Props> = ({ className }) => (
-  <section classList={{ [styles.section]: true, [className || '']: !!className }}>
-    <Image source={avatar} />
-    <span>{Author}</span>
-  </section>
-)
+export const User: Component<Props> = ({ className }) => {
+  const [links, setLinks] = createSignal([
+    { Icon: Email, url: Links.EMAIL },
+    { Icon: Github, url: Links.GITHUB },
+    { Icon: Linkedin, url: Links.LINKEDIN },
+  ])
+
+  onMount(() => void setLinks(([{ Icon, url }, ...rest]) => [{ Icon, url: atob(url) as Links }, ...rest ]))
+
+  return (
+    <section classList={{ [styles.section]: true, [className || '']: !!className }}>
+      <Image source={avatar} />
+      <h3>{Author}</h3>
+      <h4>{'Software Developer'}</h4>
+      <div>
+        <For each={links()} children={({ Icon, url }) => <a target='_blank' rel='noopener' href={url}>{<Icon />}</a>} />
+      </div>
+    </section>
+  )
+}
--- src/types/consts.ts
+++ src/types/consts.ts
@@ -1,8 +1,9 @@
 export const Author = 'Andrew Nyzhnyk';
 
 export enum Links {
-  RESUME = 'https://ln.awkwrd.dev/~cv',
   BLOG = 'https://ln.awkwrd.dev/~md',
+  EMAIL = 'bWFpbHRvOmFuZHJld0Bhd2t3cmQuZGV2',
   GITHUB = 'https://ln.awkwrd.dev/~gh',
-  LINKEDIN = 'https://ln.awkwrd.dev/~in'
+  LINKEDIN = 'https://ln.awkwrd.dev/~in',
+  RESUME = 'https://ln.awkwrd.dev/~cv'
 }
