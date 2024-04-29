ea91414a270aa2be03780292a8377e00173ba846
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2024-04-29 11:32:38 +0200

    improved a11y

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -12,12 +12,12 @@ type Props = {
 
 export const User: Component<Props> = ({ className }) => {
   const [links, setLinks] = createSignal([
-    { Icon: Email, url: Links.EMAIL },
-    { Icon: Github, url: Links.GITHUB },
-    { Icon: Linkedin, url: Links.LINKEDIN },
+    { Icon: Email, url: Links.EMAIL, label: 'Email' },
+    { Icon: Github, url: Links.GITHUB, label: 'GitHub' },
+    { Icon: Linkedin, url: Links.LINKEDIN, label: 'LinkedIn' }
   ])
 
-  onMount(() => void setLinks(([{ Icon, url }, ...rest]) => [{ Icon, url: atob(url) as Links }, ...rest ]))
+  onMount(() => void setLinks(([{ Icon, url, label }, ...rest]) => [{ Icon, label, url: atob(url) as Links }, ...rest ]))
 
   return (
     <section classList={{ [styles.section]: true, [className || '']: !!className }}>
@@ -25,7 +25,7 @@ export const User: Component<Props> = ({ className }) => {
       <h3>{Author}</h3>
       <h4>{'Software Developer'}</h4>
       <div>
-        <For each={links()} children={({ Icon, url }) => <a target='_blank' rel='noopener' href={url}>{<Icon />}</a>} />
+        <For each={links()} children={({ Icon, label, url }) => <a target='_blank' rel='noopener' aria-label={label} href={url}>{<Icon />}</a>} />
       </div>
     </section>
   )
